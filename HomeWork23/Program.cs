2feebedb959367bde47a23c6b6e7d8bd7bcc919b
borisimus <borisimusb@yandex.ru>
2022-08-31 16:32:02 +0500

    Home Work 23

---
+++ HomeWork23/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+void GetPowCube(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        if (i >= number)
+        {
+        Console.Write(Math.Pow (i, 3) + ".");
+        }
+        else
+        {
+        Console.Write(Math.Pow(i, 3) + ", ");
+        }
+    }
+}
+Console.Write("Введите число: ");
+GetPowCube(Convert.ToInt32(Console.ReadLine()));
